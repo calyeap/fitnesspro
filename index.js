3729e3a68a2c8afe2580aaea15098af4a16b6f7a
calyeap <calvinyeap.h@gmail.com>
2019-06-11 21:30:28 +0800

    changed home index

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const express = require("express");
 const methodOverride = require("method-override");
 const pg = require("pg");
 
-
 // inside of db.js
 
 //require the url library
 //this comes with node, so no need to yarn add
-const url = require('url');
+const url = require("url");
 
 //check to see if we have this heroku environment variable
-if( process.env.DATABASE_URL ){
-
+if (process.env.DATABASE_URL) {
   //we need to take apart the url so we can set the appropriate configs
 
   const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
+  const auth = params.auth.split(":");
 
   //make the configs object
   var configs = {
@@ -23,29 +21,24 @@ if( process.env.DATABASE_URL ){
     password: auth[1],
     host: params.hostname,
     port: params.port,
-    database: params.pathname.split('/')[1],
+    database: params.pathname.split("/")[1],
     ssl: true
   };
-
-}else{
-
+} else {
   //otherwise we are on the local network
   var configs = {
-      user: 'cyeap',
-      host: '127.0.0.1',
-      database: 'fitness',
-      port: 5432
+    user: "cyeap",
+    host: "127.0.0.1",
+    database: "fitness",
+    port: 5432
   };
 }
-
 
 const pool = new pg.Pool(configs);
 
 pool.on("error", function(err) {
   console.log("idle client error", err.message, err.stack);
 });
-
-
 
 /**
  * ===================================
@@ -64,7 +57,7 @@ app.use(
 );
 
 app.use(methodOverride("_method"));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Set react-views to be the default view engine
 const reactEngine = require("express-react-views").createEngine();
@@ -87,23 +80,22 @@ app.use(cookieParser());
  * ===========================================
  */
 
- //REGISTRATION//
-app.post('/registration', (request, response) => {
-  console.log( request.body );
+//REGISTRATION//
+app.post("/registration", (request, response) => {
+  console.log(request.body);
 
-  let query = 'INSERT INTO users (username, password) VALUES ($1, $2)';
+  let query = "INSERT INTO users (username, password) VALUES ($1, $2)";
 
   const values = [request.body.username, sha256(request.body.password)];
 
   pool.query(query, values, (errorObj, result) => {
-
-    if( errorObj ){
-      console.log( "Something went wrong!");
-      console.log( errorObj );
+    if (errorObj) {
+      console.log("Something went wrong!");
+      console.log(errorObj);
     }
 
     console.log("Query done");
-    response.redirect('/login');
+    response.redirect("/login");
   });
 });
 
@@ -120,15 +112,14 @@ app.post("/login", (request, response) => {
       // name is correct
       if (sha256(request.body.password) === result.rows[0].password) {
         // password is correct
-        response.cookie('currentUser',request.body.username)
+        response.cookie("currentUser", request.body.username);
         response.cookie("loggedIn", true);
         response.redirect("/home");
-        }else{
-          response.render('login',{donald:'trump'});
-        }
-
-    }else{
-      response.render('login',{donald:'trump'});
+      } else {
+        response.render("login", { donald: "trump" });
+      }
+    } else {
+      response.render("login", { donald: "trump" });
     }
   });
 });
@@ -169,15 +160,18 @@ app.delete("/home/deleteRow/:id", (request, response) => {
   });
 });
 
-
-
 //New Form Page
 app.post("/activities/new", (request, response) => {
   console.log(request.body);
 
-  let query = "INSERT INTO activities (description, date, username) VALUES ($1, $2, $3)";
+  let query =
+    "INSERT INTO activities (description, date, username) VALUES ($1, $2, $3)";
 
-  const values = [request.body.description, request.body.date, request.body.username];
+  const values = [
+    request.body.description,
+    request.body.date,
+    request.body.username
+  ];
 
   pool.query(query, values, (errorObj, result) => {
     if (errorObj) {
@@ -190,9 +184,14 @@ app.post("/activities/new", (request, response) => {
   });
 });
 
-//Edit Form Page 
+//Edit Form Page
 app.post("/activities/:id/edit", (request, response) => {
-  const values = [request.body.description, request.body.date, request.body.username, request.params.id];
+  const values = [
+    request.body.description,
+    request.body.date,
+    request.body.username,
+    request.params.id
+  ];
   const query = `UPDATE activities
                     SET description = $1, date = $2, username = $3
                     WHERE id = $4 RETURNING *`;
@@ -209,40 +208,39 @@ app.post("/activities/:id/edit", (request, response) => {
 });
 
 //Tick
-app.put('/activities/changeTick/:id/:completion', (request, response) => {
+app.put("/activities/changeTick/:id/:completion", (request, response) => {
+  console.log(request.params);
 
-    console.log(request.params)
+  const newData = request.params.completion == "❌" ? true : false;
 
-    const newData = request.params.completion == '❌' ? true : false;
+  let query = `UPDATE activities SET completion = ${newData} WHERE id = ${
+    request.params.id
+  }`;
 
-    let query = `UPDATE activities SET completion = ${newData} WHERE id = ${request.params.id}`;
-
-    pool.query(query, (errorObj, result) => {
-
-    if( errorObj ){
-      console.log( "REEEEEAID");
-      console.log( errorObj );
+  pool.query(query, (errorObj, result) => {
+    if (errorObj) {
+      console.log("REEEEEAID");
+      console.log(errorObj);
     }
 
     console.log("Workout completed");
-    response.redirect('/home');
+    response.redirect("/home");
   });
-
-})
+});
 
 const activitiesInfo = (user, response) => {
-console.log(user)
-  const queryStr = `SELECT * FROM activities WHERE username = '${user}' ORDER BY id ASC`
+  console.log(user);
+  const queryStr = `SELECT * FROM activities WHERE username = '${user}' ORDER BY id ASC`;
 
-  pool.query(queryStr,(err,res)=>{
-    if(err){
+  pool.query(queryStr, (err, res) => {
+    if (err) {
       console.log("Something went wrong!!!!");
       console.log(err);
     }
 
-  response.render('home',{activities:res.rows})
-  })
-}
+    response.render("home", { activities: res.rows });
+  });
+};
 
 /**
  * ===================================
@@ -250,20 +248,20 @@ console.log(user)
  * ===================================
  */
 
-app.get('/home', (request, response) => {
-activitiesInfo(request.cookies.currentUser,response);
+app.get("/", (request, response) => {
+  activitiesInfo(request.cookies.currentUser, response);
 });
 
-app.get('/registration', (request, response) => {
-    response.render('registration');
+app.get("/registration", (request, response) => {
+  response.render("registration");
 });
 
-app.get('/login', (request, response) => {
-  response.render('login');
+app.get("/login", (request, response) => {
+  response.render("login");
 });
 
 app.get("/activities/new", (request, response) => {
-  response.render('new');
+  response.render("new");
 });
 app.get("/activities/:id/edit", (request, response) => {
   response.render("edit");
@@ -272,18 +270,19 @@ app.delete("/activities/:id", (request, response) => {
   response.render("delete");
 });
 
-app.get('/logout', (request, response) => {
-    response.cookie('currentUser', null);
-    response.cookie('loggedIn', false);
-    response.render('login');
+app.get("/logout", (request, response) => {
+  response.cookie("currentUser", null);
+  response.cookie("loggedIn", false);
+  response.render("login");
 });
 
 // handle requests
 
-
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));
+const server = app.listen(PORT, () =>
+  console.log("~~~ Tuning in to the waves of port " + PORT + " ~~~")
+);
 
 let onClose = function() {
   console.log("closing");
